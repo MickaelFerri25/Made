9658b253d6ddaa72ec78cc325d5edb4643ffc742
Mickael Ferri <michaelferri664@gmail.com>
2020-06-10 22:06:24 +0200

    mise à jour des dossiers

--- src/controllers/web.controller.ts
+++ src/controllers/web.controller.ts
@@ -3,3 +3,27 @@ import express from 'express';
 export const home = (req: express.Request, res: express.Response) => {
   return res.render('pages/home.njk');
 };
+
+export const comment = (req: express.Request, res: express.Response) => {
+  return res.render('pages/commentaires.njk');
+};
+
+export const connexion = (req: express.Request, res: express.Response) => {
+  return res.render('pages/connexion.njk');
+};
+
+export const inscription = (req: express.Request, res: express.Response) => {
+  return res.render('pages/inscription.njk');
+};
+
+export const frontend = (req: express.Request, res: express.Response) => {
+  return res.render('pages/frontend.njk');
+};
+
+export const upload = (req: express.Request, res: express.Response) => {
+  return res.render('pages/upload.njk');
+};
+
+export const releases = (req: express.Request, res: express.Response) => {
+  return res.render('pages/releases.njk');
+};
--- src/routes/web.route.ts
+++ src/routes/web.route.ts
@@ -4,5 +4,10 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', controllers.web.home);
-
+router.get('/commentaires', controllers.web.comment);
+router.get('/connexion', controllers.web.connexion);
+router.get('/inscription', controllers.web.inscription);
+router.get('/frontend', controllers.web.frontend);
+router.get('/upload', controllers.web.upload);
+router.get('/releases', controllers.web.releases);
 export default router;
