0a8bf89f33173332baca63ba16eb5ac43bed0b42
Polo <pablo_mo@orange.fr>
2020-07-08 21:27:41 +0200

    ADD server and FIX register link

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import getServer from './server';
 
 (async () => {
   const server = await getServer();
-  server.listen(config.port, () => {
-    console.log(`Server is listenning on port ${config.port}`);
+  server.listen(config.port, config.ip, () => {
+    console.log(`Server is listenning on port ${config.port} and on ip ${config.ip}`);
   });
 })();
