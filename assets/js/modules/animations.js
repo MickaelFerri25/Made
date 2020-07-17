daaee9874a4f8415179b86d4e760cbe69781b2f5
MickaelFerri25 <mickael.github@gmail.com>
2020-07-17 16:21:43 +0200

    Refonte de la page d'upload

--- assets/js/modules/animations.js
+++ assets/js/modules/animations.js
@@ -2,5 +2,8 @@ const blockVoirPlus = document.querySelector('#invisible')
 const lienVoirPlus = document.querySelector('#voir-plus')
 const iLienVoirPlus = document.querySelector('#i-voir-plus')
 
-
+lienVoirPlus.addEventListener('click', () => {
+    blockVoirPlus.classList.toggle('visible')
+    iLienVoirPlus.classList.toggle('rotate')
+})
 
