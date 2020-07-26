e7d3cedc2f857880ddefec617e7f8c54ab11449d
Polo <pablo_mo@orange.fr>
2020-07-26 15:20:58 +0200

    ADD premium categories

--- src/services/projectcategory.service.ts
+++ src/services/projectcategory.service.ts
@@ -4,8 +4,11 @@ import { GetAllSuccess } from './results/projectcategory.result';
 import ProjectCategoryEntity from '../models/entities/projectcategory.entity';
 
 export default class ProjectCategoryService extends Service {
-  public getAll = async (): Promise<ServiceResult<GetAllSuccess>> => {
-    const categories = await ProjectCategoryEntity.findMany(this.context).exec();
-    return this.success<GetAllSuccess>({ categories });
+  public getAll = async () => {
+    const categories = await ProjectCategoryEntity.findMany(this.context).where('premiumReserved', '=', '0').exec();
+    const premiumCategories = await ProjectCategoryEntity.findMany(this.context)
+      .where('premiumReserved', '=', '1')
+      .exec();
+    return { categories, premiumCategories };
   };
 }
